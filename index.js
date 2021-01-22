eb7d24d9310743f36683be657b689dfa0aac0275
ZedoXmv <75579178+ZedoXmv@users.noreply.github.com>
2021-01-22 11:06:10 +0500

    Used Class and Objects

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-const Items = [];
+const cartObject = {
+    items: [],
+};
 
 let grandTotal = 0;
-  
-function addItem(Name,Qty,Rate) {
-    Items.push({
-        Qty,
-        Name,
-        Rate,
-        Total: Qty*Rate
-    });
+
+class CartClass {
+    constructor(){
+        this.items = [];
+    }
+    addItem(Name,Qty,Rate) {
+        this.items.push({
+            Qty,
+            Name,
+            Rate,
+            Total: Qty*Rate
+        });
+    }
+
+    getItems(){
+        return this.items
+    }
+    getNumberOfItems(){
+        return this.items.length
+    }
+    getTotalAmount(){
+        let cartTotal = 0;
+        for (const item of this.items) {
+            cartTotal += item.Total;
+        }
+        return cartTotal;
+    }
+
 }
-function addToTotal(value) {
-    grandTotal += value;
-}
+
+const cart = new CartClass();
+cart.addItem('eggs',12,1.5);
+cart.addItem('bread',3,24);
+cart.addItem('milk',4,15);
+cart.addItem('pizza',1,100);
 
 
-addItem('eggs',12,1.5);
-addItem('bread',3,24);
-addItem('milk',4,15);
-addItem('pizza',1,100);
-
-//log items
-console.log('You have ' + Items.length + ' items in your cart');
-console.log('---------------------------------');
-console.log('The items in your cart are:');
-console.table(Items);
-
-//Calucate and log total
-Items.forEach(Item => addToTotal(Item.Total));
-console.log("Your grand total is " + grandTotal);
+console.log('You have ' + cart.getNumberOfItems() + ' items in your cart');
+console.table(cart.getItems())
+console.log("Your grand total is " + cart.getTotalAmount());
