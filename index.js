38ad167069e51ace4a64c3bf6460a2f8431595ac
ZedoXmv <75579178+ZedoXmv@users.noreply.github.com>
2021-01-24 00:40:19 +0500

    Separated Items object and added gst functionality

--- index.js
+++ index.js
@@ -1,50 +1,106 @@
-class CartClass {
-    constructor(){
-        this.items = [];
-    }
-    addItem(Name,Qty,Rate) {
-        this.items.push({
-            Qty,
-            Name,
-            Rate,
-            Total: Qty*Rate
-        });
-    }
+ItemsData = [{
+		itemCode: 1,
+		itemName: 'eggs',
+		itemRate: 1.5,
+		GSTApplicable: true
+	},
+	{
+		itemCode: 2,
+		itemName: 'bread',
+		itemRate: 24,
+		GSTApplicable: false
+	},
+	{
+		itemCode: 3,
+		itemName: 'milk',
+		itemRate: 15,
+		GSTApplicable: false
+	},
+	{
+		itemCode: 4,
+		itemName: 'pizza',
+		itemRate: 100,
+		GSTApplicable: true
+	}
+];
 
-    getItems(){
-        return this.items
-    }
-    getNumberOfItems(){
-        return this.items.length
-    }
-    getTotalAmount(){
-        let cartTotal = 0;
-        for (const item of this.items) {
-            cartTotal += item.Total;
-        }
-        return cartTotal;
-    }
-    removeItem(name){
-        let removeIndex = this.items.map(function (item) {
-            return item.Name;
-        }).indexOf(name);
-        if (removeIndex === -1) {
-            //No match in cart
-            return false;
-        }
-        this.items.splice(removeIndex,1);
-    }
+function displayAllItems() {
+	console.table(ItemsData);
+}
+
+class Carts {
+	constructor() {
+		this._cartItems = [];
+		this.gstRate = 6; //6%
+	}
+	addItem(ItemCode,Qty) {
+		let itemIndex = ItemsData.map(function(item) { 
+			return item.itemCode; 
+		}).indexOf(ItemCode);
+		// console.log(ItemsData[itemIndex]);
+		let Total =  Qty * ItemsData[itemIndex].itemRate;
+		let GST = 0;
+		if (ItemsData[itemIndex].GSTApplicable) {
+			GST = Math.round((((Total/100)*this.gstRate) + Number.EPSILON) * 100) / 100; //Fancy Math to calculate and round gst to 2 dp
+		}
+		this._cartItems.push({
+			'Item Code': ItemsData[itemIndex].itemCode,
+			Qty,
+			Name: ItemsData[itemIndex].itemName,
+			Rate: ItemsData[itemIndex].itemRate,
+			Total,
+			GST
+		});
+	}
+	get cartItems(){
+		return this._cartItems;
+	}
+	getNumberOfItems() {
+		return this._cartItems.length
+	}
+	get cartTotal() {
+		let _cartTotal = 0;
+		for (const item of this._cartItems) {
+			_cartTotal += item.Total;
+		}
+		return _cartTotal;
+	}
+	get gstTotal() {
+		let _cartGST = 0;
+		for (const item of this._cartItems) {
+			_cartGST += item.GST;
+		}
+		return _cartGST;
+	}
+	removeItem(ItemCode) {
+		let removeIndex = this._cartItems.map(function (item) {
+			return item['Item Code'];
+		}).indexOf(ItemCode);
+		if (removeIndex === -1) {
+			//No match in cart
+			return false;
+		}
+		this._cartItems.splice(removeIndex, 1);
+	}
 
 }
 
-const cart = new CartClass();
-cart.addItem('eggs',12,1.5);
-cart.addItem('pizza',1,100);
-cart.addItem('bread',3,24);
-cart.addItem('milk',4,15);
+//displayAllItems();
+//console.table(ItemsData);
+const Cart = new Carts();
 
-cart.removeItem('pizza');
+Cart.addItem(1,12);
+Cart.addItem(2,3);
+Cart.addItem(3,4);
+Cart.addItem(4,1);
 
-console.log('You have ' + cart.getNumberOfItems() + ' items in your cart');
-console.table(cart.getItems());
-console.log("Your grand total is " + cart.getTotalAmount());
+Cart.removeItem(4);
+
+
+console.log('You have ' + Cart.getNumberOfItems() + ' items in your cart');
+console.table(Cart.cartItems);
+console.log(`Subtotal: ${Cart.cartTotal}`);
+console.log(`+${Cart.gstRate}% GST: ${Cart.gstTotal}`);
+console.log(`GrandTotal: ${Cart.cartTotal + Cart.gstTotal}`);
+
+console.log('-------------------------------------------------------');
